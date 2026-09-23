52e1a3bf6ce6965c51ccc56dd2cd4aff21bb2319
LethaboMaditsi <st10496347@rcconnect.edu.za>
2026-09-23 17:51:54 +0200

    Assignment start up

--- src/main/java/com/mycompany/assignment2/Assignment2.java
+++ src/main/java/com/mycompany/assignment2/Assignment2.java
@@ -11,6 +11,6 @@ package com.mycompany.assignment2;
 public class Assignment2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Git Testing");
     }
 }
