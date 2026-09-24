65d794bf8bc5bf9aaba8445fda919856a719b7d4
LethaboMaditsi <st10496347@rcconnect.edu.za>
2026-09-24 11:59:40 +0200

    Add orphaned animal rescue class
    Added the OrphanedAnimalRescue class with estimated age, feeding cost, foster care handling, rescue cost calculation, priority calculation and rescue type.

--- src/main/java/com/mycompany/assignment2/OrphanedAnimalRescue.java
+++ src/main/java/com/mycompany/assignment2/OrphanedAnimalRescue.java
@@ -1,13 +1,86 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.assignment2;
 
 /**
- *
- * @author User
+ * Represents a rescue case involving an orphaned animal.
  */
-public class OrphanedAnimalRescue {
-    
+public class OrphanedAnimalRescue extends RescueCase {
+
+    // Additional information for an orphaned animal
+    private int estimatedAge;
+    private double feedingCost;
+    private boolean fosterCareRequired;
+
+    // Constructor
+    public OrphanedAnimalRescue(String rescueCaseId, String animalName,
+            String species, String rescueLocation, String assignedRanger,
+            int numberOfRescueDays, double dailyCareCost,
+            String currentRescueStatus, int estimatedAge,
+            double feedingCost, boolean fosterCareRequired) {
+
+        super(rescueCaseId, animalName, species, rescueLocation,
+                assignedRanger, numberOfRescueDays, dailyCareCost,
+                currentRescueStatus);
+
+        this.estimatedAge = estimatedAge;
+        this.feedingCost = feedingCost;
+        this.fosterCareRequired = fosterCareRequired;
+    }
+
+    // Getters and setters
+    public int getEstimatedAge() {
+        return estimatedAge;
+    }
+
+    public void setEstimatedAge(int estimatedAge) {
+        this.estimatedAge = estimatedAge;
+    }
+
+    public double getFeedingCost() {
+        return feedingCost;
+    }
+
+    public void setFeedingCost(double feedingCost) {
+        this.feedingCost = feedingCost;
+    }
+
+    public boolean isFosterCareRequired() {
+        return fosterCareRequired;
+    }
+
+    public void setFosterCareRequired(boolean fosterCareRequired) {
+        this.fosterCareRequired = fosterCareRequired;
+    }
+
+    // Calculate the total rescue cost
+    @Override
+    public double calculateTotalRescueCost() {
+
+        double totalCost = (getNumberOfRescueDays() * getDailyCareCost())
+                + feedingCost;
+
+        if (fosterCareRequired) {
+            totalCost = totalCost + 2500.00;
+        }
+
+        return totalCost;
+    }
+
+    // Determine rescue priority
+    @Override
+    public String determineRescuePriority() {
+
+        if (fosterCareRequired && estimatedAge <= 1) {
+            return "High";
+        } else if (estimatedAge <= 3) {
+            return "Medium";
+        } else {
+            return "Low";
+        }
+    }
+
+    // Return the rescue type
+    @Override
+    public String getRescueType() {
+        return "Orphaned Animal Rescue";
+    }
 }
